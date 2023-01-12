e1678d5623cf3296bfb281033d654783f81aa70c
peak2 <36886305+peak2@users.noreply.github.com>
2023-01-12 16:34:39 +0100

    Delete src directory

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const App = () => {
-  return (
-    <div className='w-screen h-screen bg-blue-400 flex justify-center items-center text-base font-bold'>App</div>
-  )
-}
-
-export default App
